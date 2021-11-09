0b1cdcb75f41fdae73cb2c36885144fcb0a83827
Alexandre Lucas <alexandrelucas.dev@gmail.com>
2021-11-08 21:19:53 -0300

    filtros por dia mes e ano

--- backend/src/model/dao/dashboardDAO.ts
+++ backend/src/model/dao/dashboardDAO.ts
@@ -2,6 +2,7 @@ import PgDatabase from "../../db.config";
 import EntidadeDominio from "../entidade/entidadeDominio.model";
 import IDAO from "./IDAO";
 
+
 export default class DashboardDAO implements IDAO {
     tabela: string = '';
     async salvar(entidade: EntidadeDominio): Promise<EntidadeDominio> {
@@ -45,7 +46,71 @@ export default class DashboardDAO implements IDAO {
     }
 
     async consultarComFiltro(entidade: any): Promise<any> {
+        console.log(entidade);
+        if(this.validateFiltro(entidade)) {
+            let dataInicio = new Date(entidade.dataInicio);
+            let dataFim = new Date(entidade.dataFim);
+            let query; 
+            let fluxoData = this.validaDatas(dataInicio, dataFim);
+            console.log(fluxoData);
+            
+            switch(fluxoData) {
+                case 1:
+                    query = `SELECT produtos.id, produtos.titulo, DATE_PART('day', pedidos.data) AS dia, to_char(pedidos.data, 'DD/TMMonth/YYYY') AS completo, categorias.descricao, SUM("pedidosProdutos".quantidade) AS total FROM "pedidosProdutos" INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id INNER JOIN categorias ON produtos."categoriaId" = categorias.id WHERE pedidos.data between $1 and $2 GROUP BY dia, completo, produtos.titulo, produtos.id, categorias.descricao ORDER BY dia, produtos.titulo`;
+                    break;
+                case 2:
+                    query = `SELECT produtos.id, produtos.titulo, DATE_PART('month', pedidos.data) AS mes, to_char(pedidos.data, 'TMMonth/YYYY') AS completo, categorias.descricao, SUM("pedidosProdutos".quantidade) AS total FROM "pedidosProdutos" INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id INNER JOIN categorias ON produtos."categoriaId" = categorias.id WHERE pedidos.data between $1 and $2 GROUP BY mes, completo, produtos.titulo, produtos.id, categorias.descricao ORDER BY mes, produtos.titulo`;
+                    break;
+                case 3:
+                    query = `SELECT produtos.id, produtos.titulo, DATE_PART('year', pedidos.data) AS completo, categorias.descricao, SUM("pedidosProdutos".quantidade) AS total FROM "pedidosProdutos" INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id INNER JOIN categorias ON produtos."categoriaId" = categorias.id WHERE pedidos.data between $1 and $2 GROUP BY completo, produtos.titulo, produtos.id, categorias.descricao ORDER BY completo, produtos.titulo`;
+                    break;
+                default:
+                    query = `SELECT produtos.id, produtos.titulo, DATE_PART('month', pedidos.data) AS mes, to_char(pedidos.data, 'TMMonth/YYYY') AS completo, categorias.descricao, SUM("pedidosProdutos".quantidade) AS total 
+                    FROM "pedidosProdutos"
+                    INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id 
+                    INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id 
+                    INNER JOIN categorias ON produtos."categoriaId" = categorias.id 
+                    GROUP BY mes, completo, produtos.titulo, produtos.id, categorias.descricao 
+                    ORDER BY mes, produtos.titulo`;
+                        break;
+
+                    }
+                let consulta = await PgDatabase.query(query, [entidade.dataInicio, entidade.dataFim]);
+                return {resultado: consulta.rows};
+        } else {
+            return {error: 'Faltando dados do input'} as EntidadeDominio;   
+        }
         return {error: 'Teste'} as EntidadeDominio;   
+    }
+
+
+    checkData(di: Date, df: Date) : boolean {
+        if((di < df) && (df <= new Date())) return true;
+        return false;
+    }
+
+    validaDatas(dataI: Date, dataF: Date) {
+
+        if(isNaN(dataI.getTime()) || isNaN(dataF.getTime())) {
+            return -1;
+        }
+
+        if (dataI.getFullYear() == dataF.getFullYear() && dataI.getUTCMonth() == dataF.getUTCMonth()) {
+            //pegar dias 
+            return 1;
+        } else if (dataI.getFullYear() == dataF.getFullYear() && dataI.getUTCMonth() != dataF.getUTCMonth()) {
+            //pegar mês
+            return 2;
+        } else {
+            //pegar ano
+            return 3
+        }
+    }
+
+    validateFiltro(entidade: any) {
+        if(!entidade.pais && !entidade.categoria && !entidade.tempoGuarda && !entidade.tipo && !entidade.dataInicio && !entidade.dataFim)
+        return false;
+        return true;
     }
 
 }
