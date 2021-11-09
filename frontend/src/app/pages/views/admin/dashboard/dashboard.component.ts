7b8593c5ca6728e36a47d55a530bd711099f3122
William R. Ferreira <willian.usp@hotmail.com>
2021-11-09 10:20:39 -0300

    Ajuste graficos dinamicos

--- frontend/src/app/pages/views/admin/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/views/admin/dashboard/dashboard.component.ts
@@ -17,16 +17,25 @@ export class DashboardComponent implements OnInit {
   @ViewChild('trocas', {static: true}) trocas: ElementRef;
       
   public chart: Chart;
-  public paises = [];
+  public paises:any = [];
   public categorias = [];
   public tempoGuarda = [2, 3, 4, 5, 6];
   public tipo = ['Tinto', 'Branco'];
   public filtroGrafico = []
+  
   public cards = [
     { icon: 'fas fa-shopping-cart', background: 'A', title: 'Pedidos recebidos', valorIni: '486', subTitle: 'Pedidos concluídos', valorFin: '351' },
     { icon: 'fas fa-tags', background: 'B', title: 'Vendas totais', valorIni: '1641', subTitle: 'Este mês', valorFin: '213' },
     { icon: 'fas fa-retweet', background: 'C', title: 'Receita', valorIni: 'R$ 12.532,00', subTitle: 'Este mês', valorFin: 'R$ 5.032' },
     { icon: 'fas fa-coins', background: 'D', title: 'Lucro total', valorIni: 'R$ 6.562,00', subTitle: 'Este mês', valorFin: 'R$ 542' },
+  ]
+  public cores = [
+    { borderColor: '#FFCC00', backgroundColor: '#FFCC00'},
+    { borderColor: 'rgb(255, 99, 132)', backgroundColor: 'rgb(255, 99, 132)'},
+    { borderColor: 'rgb(255, 205, 86)', backgroundColor: 'rgb(255, 205, 86)'},
+    { borderColor: 'rgb(54, 162, 235)', backgroundColor: 'rgb(54, 162, 235)'},
+    { borderColor: 'purple', backgroundColor: 'purple'},
+    { borderColor: 'yellow', backgroundColor: 'yellow'}              
   ]
 
   filtroPais = new FormControl();
@@ -49,7 +58,7 @@ export class DashboardComponent implements OnInit {
     this.getListaCategorias(); 
     this.gerarGraficoVendas();
     this.gerarGraficoPesquisa();    
-  }
+  }  
 
   getDadosDashBoard() {
     this.servicoService.getDashboard().subscribe((result: any) => {
@@ -80,12 +89,20 @@ export class DashboardComponent implements OnInit {
 
   getListaPaises(){
     this.servicoService.getListaPaises().subscribe((result:any) => {
-      this.paises = result.paises;      
+      this.paises = result.paises; 
+      this.setColors();
     });
   }
 
+  setColors(){
+    for(let i = 0; i < this.paises.length; i++){      
+      this.paises[i].borderColor = this.cores[i].borderColor
+      this.paises[i].backgroundColor = this.cores[i].backgroundColor
+    }
+  }
+
   getPaisNome(paisId){
-    return this.paises.filter(p => p.id == paisId)[0].descricao;
+    return this.paises.filter(p => p.id == paisId)[0]?.descricao;
   }
 
   getCategoriaNome(categoriaId){
@@ -141,65 +158,56 @@ export class DashboardComponent implements OnInit {
   }
 
   filtrarGrafico(){
-    // console.log(this.filtroPais.value)
-    // console.log(this.filtroCategoria.value)
-    // console.log(this.filtroTipo.value)
-    // console.log(this.filtroGuarda.value)
-    console.log(moment(this.range.value.start).format('YYYY-MM-DD').replace('Invalid date', ''))
-    console.log(moment(this.range.value.end).format('YYYY-MM-DD').replace('Invalid date', ''))
-
-    let filtro = {
+    let filtros = {
       pais : this.filtroPais.value ?? [],
       categoria : this.filtroCategoria.value ?? [],
       tempoGuarda : this.filtroGuarda.value ?? [],
       tipo : this.filtroTipo.value ?? [],
       dataInicio : moment(this.range.value.start).format('YYYY-MM-DD').replace('Invalid date', ''),
       dataFim : moment(this.range.value.end).format('YYYY-MM-DD').replace('Invalid date', ''),
-    }
-
-    console.log(filtro);
-      
-    this.servicoService.filtrarGrafico(filtro).subscribe((result: any) => {
-      
-      //console.log(Object.keys(result.result))
-
-      let eixoX = Object.keys(result.result);
-      //this.filtroGrafico = result.resultado;
-
-      this.paises.forEach(p => { 
-        console.log(p)
-        console.log(result.result);
-      })
-      
-
-      this.atualizandoDadosGrafico();
+    }    
+    
+    //GET DADOS DB
+    this.servicoService.filtrarGrafico(filtros).subscribe((result: any) => {
+      this.preparaDadosDB(result, filtros);    
     });
   }
 
-  atualizandoDadosGrafico(){
-    let eixoX = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"];
-    let datasets = [
-      {
-        label: 'Portugues',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [29, 41, 56, 34, 36],
-      },
-      {
-        label: 'Frances',
-        borderColor: '#FFCC00',
-        backgroundColor: '#FFCC00',
-        data: [26, 38, 31, 19, 56],
-      },
-    ]
+  //AJUSTA RETORNO DO DB E MONTA DADOS PARA O GRAFICO
+  preparaDadosDB(resultDB, filtro){
+    let eixoX = Object.keys(resultDB.result);
+    this.paises.forEach(p => {
+      p.data = [];
+      Object.keys(resultDB.result).forEach(filtro => {
+        resultDB.result[filtro].filter(dia => dia.pais == p.descricao).length ? 
+          p.data.push(parseInt(resultDB.result[filtro].filter(dia => dia.pais == p.descricao)[0].total)) : p.data.push(0)          
+      })
+    })    
+    
+    //AJUSTA OBJETO DE DADOS PARA O PONTOS DO GRAFICO
+    let dados:any = []
+    this.paises.forEach(p => {        
+      dados.push({ label : p.descricao, borderColor : p.borderColor, backgroundColor : p.backgroundColor, data : p.data })
+    });
 
-    this.addData(this.chart, eixoX, datasets)
+    //EXECUTA FILTRO DE PAISES SELECIONADOS
+    if(filtro.pais.length){
+      const paisesFiltrados = new Set(dados);      
+      filtro.pais.filter((name) => {      
+        return paisesFiltrados.has(name);
+      });
+    }
+
+    //ATUALIZA GRAFICO
+    this.updateChart(this.chart, eixoX, dados)
   }
 
-  addData(chart, label, dataSet) {    
-    chart.data.labels = label;    
-    chart.data.datasets = dataSet;   
+  updateChart(chart, eixoX, dados) {
+    chart.data.labels = eixoX;    
+    chart.data.datasets = [];
+    for(let i = 0; i < dados.length; i++){
+      chart.data.datasets.push(dados[i])
+    }
     chart.update();
   }
 }
-
