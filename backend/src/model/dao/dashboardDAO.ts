d29c354083a363e270668899640a168b06c2fc38
Alexandre Lucas <alexandrelucas.dev@gmail.com>
2021-11-08 23:22:49 -0300

    recode filtros

--- backend/src/model/dao/dashboardDAO.ts
+++ backend/src/model/dao/dashboardDAO.ts
@@ -46,11 +46,12 @@ export default class DashboardDAO implements IDAO {
     }
 
     async consultarComFiltro(entidade: any): Promise<any> {
-        console.log(entidade);
         if(this.validateFiltro(entidade)) {
             let dataInicio = new Date(entidade.dataInicio);
             let dataFim = new Date(entidade.dataFim);
             let query; 
+            let queryData;
+            let selectedDate;
             let fluxoData = this.validaDatas(dataInicio, dataFim);
 
             let listaPaises: any[] = entidade.pais;
@@ -63,65 +64,49 @@ export default class DashboardDAO implements IDAO {
             let filtroTempoGuarda = listaTempoGuarda.map( (v, index) => (index != 0 ? 'OR' : 'AND') + ` produtos."tempoGuarda" = '${v}' `).reduce( (accum, curr) => accum + curr ,'');
             let filtroCategoria = listaCategoria.map( (v, index) => (index != 0 ? 'OR' : 'AND') + ` produtos."categoriaId" = '${v}' `).reduce( (accum, curr) => accum + curr ,'');
             
+
+            
+
             switch(fluxoData) {
                 case 1:
-                    query = 
-                    `SELECT produtos.id, produtos.titulo, DATE_PART('day', pedidos.data) AS dia,
-                     to_char(pedidos.data, 'DD/TMMonth/YYYY') AS completo, categorias.descricao,
-                     SUM("pedidosProdutos".quantidade) AS total 
-                     FROM "pedidosProdutos" 
-                     INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id 
-                     INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id 
-                     INNER JOIN categorias ON produtos."categoriaId" = categorias.id 
-                     WHERE pedidos.data between $1 and $2 ${filtroPais} ${filtroTipo} ${filtroCategoria} ${filtroTempoGuarda}
-                     GROUP BY dia, completo, produtos.titulo, produtos.id, categorias.descricao 
-                     ORDER BY dia, produtos.titulo`;
-                     console.log(query);
+                    queryData = `DATE_PART('day', pedidos.data) AS dia`;
+                    selectedDate = 'dia;'
                     break;
                 case 2:
-                    query = 
-                    `SELECT produtos.id, produtos.titulo, DATE_PART('month', pedidos.data) AS mes,
-                     to_char(pedidos.data, 'TMMonth/YYYY') AS completo, categorias.descricao, 
-                     SUM("pedidosProdutos".quantidade) AS total 
-                     FROM "pedidosProdutos" 
-                     INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id 
-                     INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id 
-                     INNER JOIN categorias ON produtos."categoriaId" = categorias.id 
-                     WHERE pedidos.data between $1 and $2 ${filtroPais} ${filtroTipo} ${filtroCategoria} ${filtroTempoGuarda}
-                     GROUP BY mes, completo, produtos.titulo, produtos.id, categorias.descricao 
-                     ORDER BY mes, produtos.titulo`;
+                    queryData = `DATE_PART('month', pedidos.data) AS mes`;
+                    selectedDate = 'mes';
                     break;
                 case 3:
-                    query = 
-                    `SELECT produtos.id, produtos.titulo, DATE_PART('year', pedidos.data) AS completo,
-                     categorias.descricao, SUM("pedidosProdutos".quantidade) AS total 
-                     FROM "pedidosProdutos" 
-                     INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id 
-                     INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id 
-                     INNER JOIN categorias ON produtos."categoriaId" = categorias.id 
-                     WHERE pedidos.data between $1 and $2 ${filtroPais} ${filtroTipo} ${filtroCategoria} ${filtroTempoGuarda}
-                     GROUP BY completo, produtos.titulo, produtos.id, categorias.descricao 
-                     ORDER BY completo, produtos.titulo`;
+                    queryData = `DATE_PART('year', pedidos.data) AS ano`;
+                    selectedDate = 'ano';
                     break;
                 default:
-                    query = 
-                    `SELECT produtos.id, produtos.titulo, DATE_PART('month', pedidos.data) AS mes,
-                    to_char(pedidos.data, 'TMMonth/YYYY') AS completo, categorias.descricao, SUM("pedidosProdutos".quantidade) AS total 
-                    FROM "pedidosProdutos"
-                    INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id 
-                    INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id 
-                    INNER JOIN categorias ON produtos."categoriaId" = categorias.id
-                    GROUP BY mes, completo, produtos.titulo, produtos.id, categorias.descricao 
-                    ORDER BY mes, produtos.titulo`;
-                        break;
+                    queryData = `DATE_PART('day', pedidos.data) AS dia`;
+                    selectedDate = 'dia'; 
+                    break;
 
-                    }
-                let consulta = fluxoData != -1 ?  await PgDatabase.query(query, [entidade.dataInicio, entidade.dataFim]) : await PgDatabase.query(query);
+            }
+                
+            let queryTest = 
+                `SELECT pais.descricao as "pais", ${queryData},
+                to_char(pedidos.data, 'DD/TMMonth/YYYY') AS completo,
+                SUM("pedidosProdutos".quantidade) AS total
+                FROM "pedidosProdutos"
+                INNER JOIN produtos ON "pedidosProdutos"."produtoId" = produtos.id
+                INNER JOIN pedidos ON "pedidosProdutos"."pedidoId" = pedidos.id
+                INNER JOIN categorias ON produtos."categoriaId" = categorias.id
+                INNER JOIN pais ON produtos."paisId" = pais.id
+                INNER JOIN vendas ON pedidos.id = vendas."pedidoId"
+                WHERE vendas."pedidoId" = pedidos.id ${ fluxoData != -1 ? 'AND pedidos.data BETWEEN $1 AND $2' : ''} 
+                ${filtroPais} ${filtroTipo} ${filtroCategoria} ${filtroTempoGuarda}
+                GROUP BY ${selectedDate}, pais, completo
+                ORDER BY ${selectedDate}, pais`;
+
+                let consulta = fluxoData != -1 ?  await PgDatabase.query(queryTest, [entidade.dataInicio, entidade.dataFim]) : await PgDatabase.query(queryTest);
                 return {resultado: consulta.rows};
         } else {
             return {error: 'Faltando dados do input'} as EntidadeDominio;   
         }
-        return {error: 'Teste'} as EntidadeDominio;   
     }
 
 
